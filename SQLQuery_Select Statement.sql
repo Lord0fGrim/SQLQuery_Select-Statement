9f244aa600ce41cd4b8ee2fb377c50751e5e6551
Lord0fGrim <115959756+Lord0fGrim@users.noreply.github.com>
2023-09-05 20:56:25 -0400

    Add files via upload

---
+++ SQLQuery_Select Statement.sql
@@ -0,0 +1,10 @@
+/*
+Select Statement 
+*, Top, Distinct, Count, As, Max, Min, Avg
+
+Different database
+[Name of the database].dbo.[table name]
+*/
+
+SELECT *
+FROM SQLTutorial.dbo.EmployeeSalary
